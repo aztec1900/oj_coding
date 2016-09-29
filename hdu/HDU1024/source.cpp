3cbbe03789caa410df1dbd34e59a2c0b658c388f
aztec1900 <anzhecheng1900@163.com>
2016-09-29 23:38:05 +0800

    HDU1024

--- hdu/HDU1024/source.cpp
+++ hdu/HDU1024/source.cpp
@@ -1,6 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <cstdio>
+#include <algorithm>
 
 #define MAXN 1000005
 
@@ -8,14 +9,21 @@ using namespace std;
 
 typedef long long ll;
 
-ll dp[MAXN];
+ll dp[MAXN], pre[MAXN];
+int a[MAXN];
 
 int main() {
 	int m, n;
 	while (scanf("%d %d", &m, &n) != EOF) {
 		memset(dp, 0, sizeof(ll)*MAXN);
+		memset(pre, 0, sizeof(ll)*MAXN);
 		for (int i = 0; i < n; ++i) {
-
+			scanf("%d", &a[i]);
+		}
+		for (int j = 0; j < m; ++j) {
+			for (int i = j; i < n; ++i) {
+				dp[i] = max(dp[i - 1], pre[i - 1]) + a[i];
+			}
 		}
 	}
 }
