7f83fad37808f825891ad2fd5f27a74cc362a8bc
aztec1900 <anzhecheng1900@163.com>
2016-09-30 11:54:26 +0800

    2016-09-30

--- hdu/HDU1024/source.cpp
+++ hdu/HDU1024/source.cpp
@@ -1,6 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <cstdio>
+#include <cstring>
 #include <algorithm>
 
 #define MAXN 1000005
@@ -13,17 +14,28 @@ ll dp[MAXN], pre[MAXN];
 int a[MAXN];
 
 int main() {
+	freopen("ut.txt", "r", stdin);
 	int m, n;
 	while (scanf("%d %d", &m, &n) != EOF) {
 		memset(dp, 0, sizeof(ll)*MAXN);
 		memset(pre, 0, sizeof(ll)*MAXN);
+		ll dpMax;
 		for (int i = 0; i < n; ++i) {
 			scanf("%d", &a[i]);
 		}
 		for (int j = 0; j < m; ++j) {
+			dpMax = INT_MIN;
 			for (int i = j; i < n; ++i) {
-				dp[i] = max(dp[i - 1], pre[i - 1]) + a[i];
+				if(i == 0)
+					dp[i] = a[i];
+				else {
+					dp[i] = max(dp[i - 1], pre[i - 1]) + a[i];
+					pre[i - 1] = dpMax;
+				}
+				dpMax = max(dpMax, dp[i]);
 			}
 		}
+		printf("%lld\n", dpMax);
 	}
+	return 0;
 }
---
+++ hdu/HDU2602/Source.cpp
@@ -0,0 +1,42 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+
+#define MAXN 1005
+
+int dp[MAXN][MAXN];
+int vol[MAXN], value[MAXN];
+
+int package01(int v, int n) {
+	if (dp[v][n] == -1) {
+		if (v == 0 || n == 0)
+			dp[v][n] = 0;
+		else {
+			if (v >= vol[n - 1])
+				dp[v][n] = max(package01(v - vol[n - 1], n - 1) + value[n - 1], package01(v, n - 1));
+			else
+				dp[v][n] = package01(v, n - 1);
+		}
+	}
+	return dp[v][n];
+}
+
+int main() {
+	freopen("ut.txt", "r", stdin);
+	int nCase, n, v;
+	scanf("%d", &nCase);
+	for (int iCase = 0; iCase < nCase; ++iCase) {
+		scanf("%d %d", &n, &v);
+		for (int i = 0; i < n; ++i)
+			scanf("%d", &value[i]);
+		for (int i = 0; i < n; ++i)
+			scanf("%d", &vol[i]);
+		memset(dp, -1, sizeof(int)*MAXN*MAXN);
+		printf("%d\n", package01(v, n));
+	}
+	return 0;
+}
